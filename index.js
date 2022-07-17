4c1162905b636bf462edc9cf5102411e93be36c1
Kirera Wainaina <richardwainainak@gmail.com>
2022-07-17 22:05:31 +0300

    Create a file paths to be passed to the stream

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http2 = require("http2");
-const fs = require("fs")
+const fs = require("fs");
+const path = require("path");
 
 const port = 443;
 
@@ -11,7 +12,32 @@ const server = http2.createSecureServer({
 server.listen(port, console.log(`Listening on port ${port}`))
 
 server.on("stream", (stream, headers) => {
-    console.log("A request was received")
-    stream.respond({ ":status": 200});
-    stream.end("Personal Website")
+    console.log(`{new Date()}, path: ${stream[":path"]}`)
+    console.log(headers);
+    const filePath = createFilePath(headers[":path"]);
+    // stream.respond({ ":status": 200});
+    console.log(filePath)
+    stream.respondWithFile(
+        filePath, 
+        { "status": 200 }, 
+        { onError });
+    
+    function onError(error) {
+        try {
+            if (error.code == "ENOENT") {
+                stream.respond({ ":status": 404 });
+            } else {
+                stream.respond({ ":status": 505})
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    // stream.end("Personal Website")
 })
+
+function createFilePath(filePath) {
+    if (filePath == "/") {
+        return path.join(__dirname, "/frontend/html/home.html");
+    }
+}

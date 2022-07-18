c2a29320b88dfcd8b9e909c951c1dff7df9a6203
Kirera Wainaina <richardwainainak@gmail.com>
2022-07-18 11:17:42 +0300

    Refactor the index.js code
    Add comments to each function

--- index.js
+++ index.js
@@ -12,31 +12,34 @@ const server = http2.createSecureServer({
 server.listen(port, console.log(`Listening on port ${port}`))
 
 server.on("stream", (stream, headers) => {
+    // Respond to incoming requests
+
     console.log(`${new Date()}, path: ${headers[":path"]}`)
     console.log(headers);
     const filePath = createFilePath(headers[":path"]);
-    // stream.respond({ ":status": 200});
-    console.log(filePath)
+    
     stream.respondWithFile(
         filePath, 
         { "status": 200 }, 
         { onError });
-    
-    function onError(error) {
-        try {
-            if (error.code == "ENOENT") {
-                stream.respond({ ":status": 404 });
-            } else {
-                stream.respond({ ":status": 505})
-            }
-        } catch (error) {
-            console.log(error)
-        }
-    }
-    // stream.end("Personal Website")
 })
 
+function onError(error) {
+    // Handle error event when responding to file
+    try {
+        if (error.code == "ENOENT") {
+            stream.respond({ ":status": 404 });
+        } else {
+            stream.respond({ ":status": 505})
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
 function createFilePath(filePath) {
+    // Match incoming request paths to files
     if (filePath == "/") {
         return path.join(__dirname, "/frontend/html/home.html");
     } else {

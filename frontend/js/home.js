ba39859d45142eddd17538197177fd82990d5107
Kirera Wainaina <richardwainainak@gmail.com>
2022-07-26 17:25:12 +0300

    Only load one video
    Also clean up the code.

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -1,23 +1,9 @@
-
-// // landscapeVideo.addEventListener("canplaythrough", () => landscapeVideo.play(), { once: true})
-// landscapeVideo.addEventListener("ended", event => toggleVideoTags(event))
-// portraitVideo.addEventListener("ended", event => toggleVideoTags(event))
-
-
-// function toggleVideoTags(event) {
-//     //restart the play
-//     event.target.play();
-
-//     const welcomeTag = document.querySelector("#welcome-container h1");
-//     const nameTag = document.querySelector("#welcome-container p");
-    
-//     welcomeTag.classList.toggle("show-element");
-//     nameTag.classList.toggle("show-element");
-// }
-var landscapeVideo, portraitVideo;
 document.addEventListener("DOMContentLoaded", () => {
-    landscapeVideo = document.getElementById("landscape");
-    portraitVideo = document.getElementById("portrait")
+    const landscapeVideo = document.getElementById("landscape");
+    const portraitVideo = document.getElementById("portrait")
+
+    landscapeVideo.addEventListener("ended", event => toggleVideoTags(event))
+    portraitVideo.addEventListener("ended", event => toggleVideoTags(event))
 
     if (isLandscape()) {
         landscapeVideo.src = "/smoke-vid-landscape.mp4"
@@ -26,20 +12,17 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 })
 
-function createVideoElement() {
-    const video = document.createElement("video");
-    video.setAttribute("muted", "");
-    if (isLandscape()) {
-        video.id = "landscape";
-        video.src = "/smoke-vid-landscape.mp4"
-    } else {
-        video.id = "portrait";
-        video.src = "/smoke-vid-portrait.mp4"
-    }
-    return video
-}
-
 function isLandscape() {
     const orientation = window.matchMedia("(orientation: landscape)");
     return orientation.matches
+}
+
+function toggleVideoTags(event) {
+    //restart the play
+    event.target.play();
+
+    const welcomeTag = document.querySelector("#welcome-container h1");
+    const nameTag = document.querySelector("#welcome-container p");  
+    welcomeTag.classList.toggle("show-element");
+    nameTag.classList.toggle("show-element");
 }

f435efb845b90e62abd05b09bd36552b1eced9c9
Kirera Wainaina <richardwainainak@gmail.com>
2022-07-26 17:02:56 +0300

    Start the video loading process from scratch

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -1,17 +1,44 @@
-const landscapeVideo = document.getElementById("landscape");
-const portraitVideo = document.getElementById("portrait")
+// const landscapeVideo = document.getElementById("landscape");
+// const portraitVideo = document.getElementById("portrait")
 
-landscapeVideo.addEventListener("ended", event => toggleVideoTags(event))
-portraitVideo.addEventListener("ended", event => toggleVideoTags(event))
+// // landscapeVideo.addEventListener("canplaythrough", () => landscapeVideo.play(), { once: true})
+// landscapeVideo.addEventListener("ended", event => toggleVideoTags(event))
+// portraitVideo.addEventListener("ended", event => toggleVideoTags(event))
 
 
-function toggleVideoTags(event) {
-    //restart the play
-    event.target.play();
+// function toggleVideoTags(event) {
+//     //restart the play
+//     event.target.play();
 
-    const welcomeTag = document.querySelector("#welcome-container h1");
-    const nameTag = document.querySelector("#welcome-container p");
+//     const welcomeTag = document.querySelector("#welcome-container h1");
+//     const nameTag = document.querySelector("#welcome-container p");
     
-    welcomeTag.classList.toggle("show-element");
-    nameTag.classList.toggle("show-element");
+//     welcomeTag.classList.toggle("show-element");
+//     nameTag.classList.toggle("show-element");
+// }
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Event ran")
+    const video = createVideoElement();
+    const body = document.querySelector("body");
+    body.appendChild(video);
+    // video.addEventListener("")
+})
+
+function createVideoElement() {
+    const video = document.createElement("video");
+    video.setAttribute("muted", "");
+    if (isLandscape()) {
+        video.id = "landscape";
+        video.src = "/smoke-vid-landscape.mp4"
+    } else {
+        video.id = "portrait";
+        video.src = "/smoke-vid-portrait.mp4"
+    }
+    return video
+}
+
+function isLandscape() {
+    const orientation = window.matchMedia("(orientation: landscape)");
+    return orientation.matches
 }

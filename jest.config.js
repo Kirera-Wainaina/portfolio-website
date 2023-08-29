64689c19befce798fdda0c65cb2e1cb92b6d6032
Kirera Wainaina <richardwainainak@gmail.com>
2023-08-29 10:17:33 +0300

    Change setup file in jest to ts

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ const createJestConfig = nextJest({ dir: './' })
  
 // Any custom config you want to pass to Jest
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 }
  
 // createJestConfig is exported in this way to ensure that next/jest can load the Next.js configuration, which is async

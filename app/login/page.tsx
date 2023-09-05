a59a893cfcd151d0716f8270145233b2d5234ce7
Kirera Wainaina <richardwainainak@gmail.com>
2023-09-05 18:47:17 +0300

    Add tailwind css classes to login elements

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,18 +2,23 @@ import * as React from "react";
 
 export default function Page() {
   return (
-    <form>
-      <h1>Login</h1>
+    <div className="flex flex-row justify-center items-center h-screen font-sans">
+      <form className="w-1/3 h-1/2 shadow-lg flex flex-col">
+        <h1 className="self-center font-serif text-2xl">Login</h1>
 
-      <label>
-        Email:
-        <input type="email" />
-      </label>
+        <label className="m-10 mb-0">
+          Email:
+          <input type="email" className="w-full h-10 border-solid border-slate-300 border-2 text-base"/>
+        </label>
 
-      <label>
-        Password:
-        <input type="password" />
-      </label>      
-    </form>
+        <label className="m-10 mb-0">
+          Password:
+          <input type="password" className="w-full h-10 border-solid border-slate-300 border-2"/>
+        </label>
+
+        <button type="submit" className="bg-blue-950 m-10 text-white h-10 rounded">Submit</button>
+      </form>
+    </div>
+
   )
 }
